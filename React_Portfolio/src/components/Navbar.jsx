e869c909c58687f6ddc3d7a1aafdce3bbae91ae2
jmacker101 <jm470t@gmail.com>
2024-07-21 04:04:28 -0400

    got my socials in the navbar

---
+++ React_Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import logo from '../assets/JeovanMillsLogo2.png';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaSquareXTwitter } from 'react-icons/fa6';
+import { FaYoutubeSquare } from 'react-icons/fa';
+const Navbar = () => {
+  return (
+    // turns back ground color for my logo and for my initals red which are Jm for Navbar
+    // mb -20 was added to change something with the navbar between red and flex
+    <nav className='bg-red-900 flex items-center justify-between py-8 max-h-9'>
+        {/* comment about changing the logo size with max width 14 */}
+    <div className='flex flex-shrink-0 items-center max-w-14'>
+    <img src={logo} alt='logo' />
+    </div>
+    {/* for the icons i will be using for my socials */}
+    <div className='m-8 flex items-center justify-center gap-4 text-2x1'>
+        <FaLinkedin />
+        <FaGithub />
+        <FaSquareXTwitter />
+        <FaYoutubeSquare />
+    
+    </div>
+   </nav>
+  )
+}
+
+export default Navbar
