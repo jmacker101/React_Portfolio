88f2b7c02b3c088b963b210863ab13b740da2914
jmacker101 <jm470t@gmail.com>
2024-07-22 14:17:42 -0400

    making a new component for technologies for a new section

--- React_Portfolio/src/components/Technologies.jsx
+++ React_Portfolio/src/components/Technologies.jsx
@@ -1,9 +1,16 @@
-
+import { RiReactjsLine } from 'react-icons/ri';
 
 const Technologies = () => {
   return (
-    <div className=''>Technologies</div>
-  )
-}
+    <div className="border-b border-neutral-800 pb-24">
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <iReactjsLine />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Technologies
+export default Technologies;
