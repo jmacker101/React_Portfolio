8359db094c6ffaf98c7c6024b509347b207fe9fa
jmacker101 <jm470t@gmail.com>
2024-07-23 04:51:40 -0400

    commiting a few things from experience to save to perform a test run on adding tags

--- React_Portfolio/src/components/Experience.jsx
+++ React_Portfolio/src/components/Experience.jsx
@@ -8,16 +8,20 @@ const Experience = () => {
         {EXPERIENCES.map((Experience, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
             <div className="w-full lg:w-1/4">
+            {/* experience.year put the year on the left side of the page */}
               <p className="mb-2 text-sm text-neutral-400">{Experience.year}</p>
             </div>
+            {/* experience.role puts the job role in the center under experience */}
               <div className="w-full max-w-xl lg:w-3/4">
               <h6 className="mb-2 font-semibold">
                 {Experience.role} -{''} 
                 <span className="text-sm text-purple-100">
+                    {/* experience.company puts the company next to the role */}
                     {Experience.company}
                     </span> 
                 </h6>
                 <p className='MB-4 text-neutral-400'>{Experience.description}</p>
+                {/* experience/technologies.map are the tags used below the descriptions */}
                 {Experience.technologies.map ((tech, index) => (
                     <span key={index}>{tech}</span>
                 
