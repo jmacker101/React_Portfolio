f28e6dc1254049cf066a3f46533880b1f7d8a1e2
jmacker101 <jm470t@gmail.com>
2024-09-03 16:12:35 -0400

    finished animating contacts

--- React_Portfolio/src/components/Contact.jsx
+++ React_Portfolio/src/components/Contact.jsx
@@ -9,8 +9,16 @@ const Contact = () => {
          transition={{ duration: 0.5 }}
         className='my-10 text-center text-4xl'>Get in Touch</motion.h2>
         <div className='text-center tracking-tighter'>
-            <p className='my-4'>{CONTACT.address}</p>
-            <p className='my-4'>{CONTACT.phoneNo}</p>
+            <motion.p 
+             whileInView={{ opacity: 1, x: 0 }}
+             initial= {{ opacity: 0, x: -100 }}
+             transition={{ duration: 1 }}
+            className='my-4'>{CONTACT.address}</motion.p>
+            {/* <motion.p 
+            whileInView={{ opacity: 1, x: 0 }}
+            initial= {{ opacity: 0, x: 100 }}
+            transition={{ duration: 1 }}
+            className='my-4'>{CONTACT.phoneNo}</motion.p> */}
             <a href='#' className='border-b'>
                 {CONTACT.email}
             </a>
